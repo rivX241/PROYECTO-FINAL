c45dbec56b02f82dd0b24b272549232e8bbdc1ec
rivX241 <ariverosv@unsa.edu.pe>
2024-12-10 10:03:44 -0500

    Creando la interfaz grafica del tablero

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Ajedrez");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GUI gp = new GUI();
+        window.add(gp);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gp.lanzarJuego();
+    }
 }
