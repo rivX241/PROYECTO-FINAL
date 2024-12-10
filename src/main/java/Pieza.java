3f5eac4f7ae8f3a37a1bb12a9c69a3179426942c
rivX241 <ariverosv@unsa.edu.pe>
2024-12-10 10:41:04 -0500

    clase padre pieza para obtener las imagenes y sus posiciones

---
+++ src/main/java/Pieza.java
@@ -0,0 +1,35 @@
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+public class Pieza {
+    public BufferedImage imagen;
+    public int x, y;
+    public int columna,fila,columnaPrevia,filaPrevia;
+    public int color;
+
+    public Pieza(int color, int columna,int fila) {
+        this.color = color;
+        this.columna = columna;
+        this.fila = fila;
+        x = posicionX(columna);
+        y = posicionY(columna);
+        columnaPrevia = columna;
+        filaPrevia = fila;
+    }
+    public BufferedImage getImagen(String rutaImagen) {
+        BufferedImage imagen = null;
+
+        try{
+            imagen = ImageIO.read(getClass().getResourceAsStream(rutaImagen + ".png"));
+        }catch(IOException e){
+            e.getMessage();
+        }
+        return imagen;
+    }
+    public int posicionX(int columna){
+        return columna * Tablero.TAMANO_CUADRADO;
+    }
+    public int posicionY(int fila){
+        return fila * Tablero.TAMANO_CUADRADO;
+    }
+}
