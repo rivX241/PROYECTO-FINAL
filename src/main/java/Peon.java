c1427c755a4e2ca4a8c7f5c6423d5d534a23e7ad
AristidesC <dcervantesapa@unsa.edu.pe>
2024-12-13 13:19:27 -0500

    comentarios clase peón

--- src/main/java/Peon.java
+++ src/main/java/Peon.java
@@ -1,23 +1,30 @@
+//Clase peon hija que hereda métodos o atributos de la superclase Pieza
 public class Peon extends Pieza{
-
+// Constructor de la clase Peón. Inicializa la pieza con el color, columna y fila en el tablero.
     public Peon(int color, int columna, int fila){
-        super(color, columna, fila);
+        
+        super(color, columna, fila); //Llama al constructor de la superclase Pieza para inicializar los atributos comunes.
 
-        tipoPieza = Tipo.PEON;
+
+        tipoPieza = Tipo.PEON; //Selecciona pieza peón
+        
+        //Da imagen según su color
         if(color == GUI.BLANCO){
             imagen = getImagen("/sprites/peonBlanco");
         }else if(color == GUI.NEGRO){
             imagen = getImagen("/sprites/peonNegro");
         }
     }
+    //Método que determina si el peón puede moverse a una casilla específica (columnaObjetivo, filaObjetivo)
     public boolean puedeMoverse(int columnaObjetivo, int filaObjetivo) {
+        //Verifica si la casilla objetivo está dentro del tablero y no es la casilla en la que ya está el peón.
         if(dentroTablero(columnaObjetivo, filaObjetivo) && !esElMismoCuadrado(columnaObjetivo, filaObjetivo)) {
             //Definir el valor del movimiento en función de su color
             int movimientoValor;
             if(color == GUI.BLANCO){
-                movimientoValor = -1;
+                movimientoValor = -1;// Peón blanco se mueve hacia arriba (fila menor)
             } else {
-                movimientoValor = 1;
+                movimientoValor = 1;// Peón negro se mueve hacia abajo (fila menor)
             }
             
             //Verificar el choque de piezas
@@ -36,18 +43,20 @@ public class Peon extends Pieza{
             //Movimiento diagonal y matar (si una pieza esta en un cuadrado diagonal al peon)
             if(Math.abs(columnaObjetivo - columnaPrevia) == 1 && filaObjetivo == filaPrevia + movimientoValor && chocaPieza != null &&
                 chocaPieza.color != color) {
-                    return true;
+                    return true; 
             }
             //Captura al paso
             if(Math.abs(columnaObjetivo-columnaPrevia) == 1 && filaObjetivo == filaPrevia + movimientoValor){
+                //Recorre todas las piezas en el tablero para ver si un peón enemigo acaba de mover 2 casillas
                 for(Pieza pieza:GUI.sPiezas){
                     if(pieza.columna == columnaObjetivo && pieza.fila == filaPrevia && pieza.dosPasos){
-                        chocaPieza = pieza;
+                        chocaPieza = pieza;// Asigna la pieza enemiga que será capturada
                         return true;
                     }
                 }
             }
         }
-        return false;
+        return false; //Si ninguna de las condiciones anteriores se cumple, el movimiento no es válido
+    
     }
 }
